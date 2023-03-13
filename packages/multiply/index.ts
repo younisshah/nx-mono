b0c4be284661f37889953cdc96c6643e602ef0be
Younis Shah <younisashah@gmail.com>
2023-03-13 17:34:27 +0800

    feat(multiply): adds default multiplier

--- packages/multiply/index.ts
+++ packages/multiply/index.ts
@@ -1,4 +1,4 @@
-export const multiply = (num: number, multiplier: number) => {
+export const multiply = (num: number, multiplier: number = 2) => {
     console.log(`Multiplying ${num} * ${multiplier}`);
     return num * multiplier;
 };
