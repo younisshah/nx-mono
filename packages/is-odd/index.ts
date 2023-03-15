68ed914a76bb8a0c958e9261bce4eeb5f4bee60e
Younis Shah <younisashah@gmail.com>
2023-03-15 09:21:56 +0800

    feat(child): logs FOOBAR

--- packages/is-odd/index.ts
+++ packages/is-odd/index.ts
@@ -1,4 +1,6 @@
-import { isEven } from "../is-even";
+import { isEven, FOOBAR } from "../is-even";
 
 console.log('odd calc')
 export const isOdd = (num: number) => !isEven(num);
+
+console.log(FOOBAR);
