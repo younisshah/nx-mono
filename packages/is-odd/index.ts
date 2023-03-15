c799dbe0bb9f14f060d9ff5f9267bf2f8036119c
Younis Shah <younisashah@gmail.com>
2023-03-15 09:15:26 +0800

    feat(is-odd): adds const

--- packages/is-odd/index.ts
+++ packages/is-odd/index.ts
@@ -2,3 +2,7 @@ import { isEven } from "../is-even";
 
 console.log('odd calc')
 export const isOdd = (num: number) => !isEven(num);
+export const FOOBAR = 10;
+function printFoobar() {
+    console.log(FOOBAR+1);
+}
