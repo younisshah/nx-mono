b2f5908150a6bc17bc137a6b315c207dfbf1f612
Younis Shah <younisashah@gmail.com>
2023-03-20 11:06:50 +0800

    feat(i-odd): removes log

--- packages/is-odd/index.ts
+++ packages/is-odd/index.ts
@@ -1,6 +1,5 @@
-import { isEven, FOOBAR } from "../is-even";
+import {isEven, FOOBAR} from "../is-even";
 
-console.log('odd calc')
 export const isOdd = (num: number) => !isEven(num);
 
 console.log(FOOBAR);
